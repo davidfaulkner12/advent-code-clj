b2480605716cf43a221ebd04d9120fcde8735472
David Wang-Faulkner <david@wangfaulkner.com>
2020-12-05 20:56:32 +0800

    Added actual problem output tests

--- test/advent_code/problem/day3_2020_2_test.clj
+++ test/advent_code/problem/day3_2020_2_test.clj
@@ -1,26 +1,9 @@
-(ns advent-code.problem.day3-2020-test
+(ns advent-code.problem.day3-2020-2-test
   (:require [clojure.test :refer :all]
             [advent-code.interfaces :as ifaces]
-            [advent-code.problem.day3-2020-1 :refer :all]))
-
-(def example-map "..##.......
-#...#...#..
-.#....#..#.
-..#.#...#.#
-.#...##..#.
-..#.##.....
-.#.#.#....#
-.#........#
-#.##...#...
-#...##....#
-.#..#...#.#
-")
-
-(def parsed-map (parse-data example-map))
-
-(deftest calculate-example-map
-  (is (= (calculate-trees (make-slope 3 1) parsed-map)
-         7)))
+            [advent-code.problem.day3-2020-1 :refer :all]
+            [advent-code.problem.day3-2020-2 :refer :all]
+            [advent-code.problem.day3-2020-1-test :refer :all]))
 
 (deftest calculate-product
   (is (= (*
@@ -30,3 +13,7 @@
           (calculate-trees (make-slope 7 1) parsed-map)
           (calculate-trees (make-slope 1 2) parsed-map))
          336)))
+
+(deftest actual-problem-2 ; Have to rename to refer all to the other test :-)
+  (is (= 9533698720
+         (ifaces/run-problem "day3-2020-2" (slurp "resources/2020-day3.input")))))
