04f530a48345a61821d32b4be80280480ab68379
David Wang-Faulkner <david@wangfaulkner.com>
2020-12-18 16:40:19 +0800

    Slightly faster implementation w/ a transient map

--- src/advent_code/problem/day15_2020.clj
+++ src/advent_code/problem/day15_2020.clj
@@ -6,13 +6,13 @@
 (defn generate-spoken-set [starting-numbers]
   (let [c (count starting-numbers)
         processed-set (into {} (for [x (range (- c 1))] {(starting-numbers x) (+ x 1)}))]
-    [processed-set (starting-numbers (- c 1)) c]))
+    [(transient processed-set) (starting-numbers (- c 1)) c]))
 
 (defn speak-next-number [[spoken-set last-number last-index]]
   (let [next-index (inc last-index)
         previous-index (get spoken-set last-number 0)
         next-number (if (= 0 previous-index) 0 (- last-index previous-index))]
-    [(assoc spoken-set last-number last-index) next-number next-index]))
+    [(assoc! spoken-set last-number last-index) next-number next-index]))
 
 (defn nth-spoken-number [starting-numbers n]
   (let [c (count starting-numbers)
