ca556c73430f9fb579bdd4184a502bba2994d589
David Wang-Faulkner <david@wangfaulkner.com>
2020-12-18 10:41:21 +0800

    Clean up imports

---
+++ src/advent_code/problem/day14_2020.clj
@@ -0,0 +1,4 @@
+(ns advent-code.problem.day14-2020
+  (:require [advent-code.interfaces :as ifaces]
+            [advent-code.data-helpers :as dh]
+            [clojure.set :as set]))
---
+++ test/advent_code/problem/day14_2020_test.clj
@@ -0,0 +1,10 @@
+(ns advent-code.problem.day14-2020-test
+  (:require [clojure.test :refer :all]
+            [advent-code.interfaces :as ifaces]
+            [advent-code.data-helpers :as dh]
+            [advent-code.problem.day14-2020 :refer :all]))
+
+;
+; (deftest actual-problem
+;   (is (= 2545
+;          (ifaces/run-problem "day13-2020" "1" (slurp "resources/2020-day13.input")))))
